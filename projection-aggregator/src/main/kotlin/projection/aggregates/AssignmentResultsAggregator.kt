56ee971b7931dfb9bd89153417d9756d80118186
Fabian <fb@something.com>
2024-10-12 13:37:28 +0700

    first draft of model and new AssigmentResultsAggregator

--- projection-aggregator/src/main/kotlin/projection/aggregates/AssignmentResultsAggregator.kt
+++ projection-aggregator/src/main/kotlin/projection/aggregates/AssignmentResultsAggregator.kt
@@ -1,13 +1,8 @@
 package projection.aggregates
 
-import arrow.core.firstOrNone
-import arrow.core.getOrElse
-import arrow.core.raise.result
-import common.model.event.AddStudentEvent
 import common.model.event.AssignmentEvent
 import common.model.event.Event
 import common.model.event.ExerciseFinishedEvent
-import common.model.event.RemoveStudentEvent
 import kotlinx.serialization.Serializable
 import projection.model.Principal
 
@@ -24,25 +19,28 @@ data class Assigment(
     val results: Map<Long, AssigmentResult> = emptyMap(),
 )
 
+// TODO: groupId
 @Serializable
 data class AssigmentResultsSnapshot(
     val assignments: Map<Long, Assigment> = emptyMap(),
 ) : Snapshot
 
+// TODO: unit test
+// TODO: should never fail and validation should be done by publisher
 class AssigmentResultsAggregator :
     BaseAggregator<AssigmentResultsSnapshot>(
-        name = "group_members",
+        name = "assignment_results",
         version = 1,
         principalTypes = listOf(Principal.Group::class),
         eventTypes =
-        setOf(
-            // TODO: needed? for validation?
-            AddStudentEvent::class,
-            // TODO: Remove Result for userId
-            RemoveStudentEvent::class,
-            ExerciseFinishedEvent::class,
-            AssignmentEvent::class,
-        ),
+            setOf(
+                // TODO: needed? for validation?
+                // AddStudentEvent::class,
+                // TODO: Remove Result for userId?
+                // RemoveStudentEvent::class,
+                ExerciseFinishedEvent::class,
+                AssignmentEvent::class,
+            ),
         snapshotSerializer = AssigmentResultsSnapshot.serializer(),
     ) {
     override fun initialSnapshot(principal: Principal): AssigmentResultsSnapshot = AssigmentResultsSnapshot()
@@ -53,39 +51,51 @@ class AssigmentResultsAggregator :
         snapshot: AssigmentResultsSnapshot,
     ): AssigmentResultsSnapshot =
         when (event) {
-            //is AddStudentEvent -> snapshot.copy(members = snapshot.members + event.userId)
-            //is RemoveStudentEvent -> snapshot.copy(members = snapshot.members - event.userId)
+            // is AddStudentEvent -> snapshot.copy(members = snapshot.members + event.userId)
+            // is RemoveStudentEvent -> snapshot.copy(members = snapshot.members - event.userId)
             is AssignmentEvent -> {
-                // TODO: check if assigment already exists?
-                snapshot.copy(
-                    assignments = snapshot.assignments + Pair(
-                        event.assignmentId,
-                        Assigment(assignmentId = event.assignmentId)
+                if (snapshot.assignments[event.assignmentId] == null) {
+                    snapshot.copy(
+                        assignments =
+                        snapshot.assignments +
+                                Pair(
+                                    event.assignmentId,
+                                    Assigment(assignmentId = event.assignmentId),
+                                ),
                     )
-                )
+                } else {
+                    snapshot
+                }
             }
 
             is ExerciseFinishedEvent -> {
-                val assigment = snapshot.assignments
-                    .getOrElse(event.assignmentId) {
-                        throw InvalidStateException(
-                            "Failed to find assigment with id ${event.assignmentId} in snapshot ${snapshot}. " +
-                                    "Missing AssignmentEvent for given assignmentId?"
-                        )
-                    }
+                val assigment =
+                    snapshot.assignments
+                        .getOrElse(event.assignmentId) {
+                            throw InvalidStateException(
+                                "Failed to find assigment with id ${event.assignmentId} in snapshot $snapshot. " +
+                                    "Missing AssignmentEvent for given assignmentId?",
+                            )
+                        }
 
                 snapshot.copy(
-                    assignments = snapshot.assignments + Pair(
-                        event.assignmentId, assigment.copy(
-                            results = assigment.results + Pair(
-                                event.assignmentId, AssigmentResult(
-                                    userId = event.userId,
-                                    numbErrors = event.numErrors,
-                                    maxErrors = event.maxErrors
-                                )
-                            )
-                        )
-                    )
+                    assignments =
+                        snapshot.assignments +
+                            Pair(
+                                event.assignmentId,
+                                assigment.copy(
+                                    results =
+                                        assigment.results +
+                                            Pair(
+                                                event.userId,
+                                                AssigmentResult(
+                                                    userId = event.userId,
+                                                    numbErrors = event.numErrors,
+                                                    maxErrors = event.maxErrors,
+                                                ),
+                                            ),
+                                ),
+                            ),
                 )
             }
 
@@ -93,6 +103,4 @@ class AssigmentResultsAggregator :
         }
 }
 
-class InvalidStateException(msg: String) : RuntimeException(msg) {
-
-}
+class InvalidStateException(msg: String) : RuntimeException(msg)
